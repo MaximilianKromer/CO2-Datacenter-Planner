ad64a0f23dcae1476a6c4760b2b13ffc19cafdac
Max Kromer <a.makro@web.de>
2023-05-31 13:18:27 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `CO2 Datacenter Planner`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
