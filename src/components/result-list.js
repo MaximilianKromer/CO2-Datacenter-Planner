6cf1d6981fd8bd56cd46c3fb5b3bb5e823e46678
Jakub Kwinta <jak.kwinta@gmail.com>
2023-06-15 19:17:35 +0200

    small changes

--- src/components/result-list.js
+++ src/components/result-list.js
@@ -6,6 +6,7 @@ import Form from "react-bootstrap/Form";
 import { countries } from "../assets/countries";
 import LineChart from "./line-chart";
 import calculateCO2History from "../utils/array-to-co2";
+import BuildDataCentre from "./builddatacentre";
 
 const ResultList = ({ datacenter }) => {
 
@@ -23,36 +24,45 @@ const ResultList = ({ datacenter }) => {
 
   return (
     <Container>
-      <h1>Countries where you should build new Data Centre</h1>
+      <h1>Your desired destinations to build Data Centre</h1>
       <br></br>
       <Form id="results">
         <ul class="list-group  list-group-numbered">
           <li class="list-group-item">
+
+            <br></br>
+            <Label>Country: &nbsp; </Label>
             {countries
               .filter((c) => c.code === "DE")
               .map((c) => (
                 <Label for={c.code}>{c.name}</Label>
               ))}
             <br></br>
-            <Label>Carbon Intensity: 202 gCO₂eq/kWh</Label>
+            <Label>Name: &nbsp; </Label>
             <br></br>
-            <Label>Costs: 10€/day</Label>
+            <Label>Carbon Intensity: &nbsp;</Label>
+            <br></br>
+            <Label>Costs: &nbsp;</Label>
             <br></br>
 						<LineChart dataArray={calculateCO2History(datacenter.kWhArray, "DE")} title="CO2" label="CO2" />
           </li>
+
+
           <li class="list-group-item">
+            <br></br>
+            <Label>Country: &nbsp;  </Label>
             {countries
               .filter((c) => c.code === "SK")
               .map((c) => (
                 <Label for={c.code}>{c.name}</Label>
               ))}
             <br></br>
-            <Label>Carbon Intensity: 222 gCO₂eq/kWh</Label>
+            <Label>Name: &nbsp; </Label>
             <br></br>
-            <Label>Costs: 11€/day</Label>
+            <Label>Carbon Intensity: &nbsp;</Label>
             <br></br>
-            
-
+            <Label>Costs: &nbsp;</Label>
+            <br></br>
 						<LineChart dataArray={datacenter.kWhArray} title="CO2" label="CO2" />
           </li>
         </ul>
